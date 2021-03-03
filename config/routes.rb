0119c152e4284947e5c81372ece5e9d8106987db
tep yith <tep.yith@gmail.com>
2021-03-02 20:46:01 -0500

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'listings/index'
-  get 'listings/show'
-  get 'listings/new'
-  get 'listings/create'
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookings, only: %i[index show new create update]
-  resources :listings, only: %i[index show new create]
+  resources :listings
 
+  resources :bookings, only: %i[index show new create update]
 end
