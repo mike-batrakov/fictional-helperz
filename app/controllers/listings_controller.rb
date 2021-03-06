3cadca2a11e32caa2c7896fe8bcbfa24c0dca0a4
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-06 14:19:51 -0500

    Add actions to the listing policy

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -17,6 +17,7 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     @listing.user = current_user
+    authorize @listing
     if @listing.save
       redirect_to listing_path(@listing)
     else
@@ -40,6 +41,7 @@ class ListingsController < ApplicationController
 
   def set_listing
     @listing = Listing.find(params[:id])
+    authorize @listing
   end
 
   def listing_params
