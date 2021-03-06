bc4e52e35b0687ffd040e7c388dfe90a32cdfa4f
tep yith <tep.yith@gmail.com>
2021-03-06 10:55:25 -0500

    Commiting new routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :listings do
     resources :bookings, only: [:new, :create]
   end
-  
-  
-  resources :bookings, only: [:edit, :update]
 
+  resources :bookings, only: [:edit, :update]
 end
